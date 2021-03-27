ce6072714a7e858d616b35fb329e0a89e4fe390a
dara <just4oludee@yahoo.com>
2021-03-27 12:03:45 -0400

    event listener attempt

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let h2 = document.createElement('h2')
 h2.textContent = "Full Stack Software Engineer"
 
 document.querySelector('body').appendChild(h2)
+
+
+const about = document.getElementsByClassName("card card-1");
+function scrollToTop() {
+    elmnt.scrollIntoView(true);
+
+}
+about.addEventListener('click', scrollToTop)
+    
+
