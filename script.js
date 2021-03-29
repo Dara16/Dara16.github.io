52b33ea1ed4dd0499d29b5698854cead5eacde16
dara <just4oludee@yahoo.com>
2021-03-28 21:24:15 -0400

    search input added

--- script.js
+++ script.js
@@ -3,12 +3,32 @@ h2.textContent = "Full Stack Software Engineer"
 
 document.querySelector('body').appendChild(h2)
 
+const searchInput = document.getElementById("search-input");
+
+const searchButton = document.getElementById("search-button");
+
+function alertInputBox() {
+    alert(searchInput.value)
+}
+
+searchButton.addEventListener('click', alertInputBox)
+
+/*function addingEventListener() {
+    const input = document.getElementById('home');
+
+    function clickAlert() {
+      alert('Welcome to my Website!');
+    }
+    
+    input.addEventListener('click', clickAlert); */
+
+
 
 const about = document.getElementsByClassName("card card-1");
-function scrollToBottom() {
+function scrollToTop() {
     elmnt.scrollIntoView(true);
 
 }
-about.addEventListener('click', scrollToBottom)
+about.addEventListener('click', scrollToTop)
     
 
